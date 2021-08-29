c13edec7f9a9e7d25765d1a83486fe574af6208d
Victor Cazacov <cazacov.git@gmail.com>
2021-08-29 23:54:35 +0200

    Day 1 - puzzle 1

---
+++ CSharp/Day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdventOfCode2015.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instructions = System.IO.File.ReadAllText("input.txt");
+            Puzzle1(instructions);
+        }
+
+        public static void Puzzle1(string instructions)
+        {
+            var floor = 0;
+            foreach (var ch in instructions)
+            {
+                if (ch == '(')
+                {
+                    floor++;
+                }
+
+                if (ch == ')')
+                {
+                    floor--;
+                }
+            }
+            Console.WriteLine($"Santa is at floor #{floor}");
+        }
+    }
+}
