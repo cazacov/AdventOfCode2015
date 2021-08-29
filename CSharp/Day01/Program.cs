26c4976416bfc9d04290577dde065370e7ba7589
Victor Cazacov <cazacov.git@gmail.com>
2021-08-30 00:19:17 +0200

    Day 2 puzzles

--- CSharp/Day01/Program.cs
+++ CSharp/Day01/Program.cs
@@ -7,29 +7,27 @@ namespace AdventOfCode2015.Day01
         static void Main(string[] args)
         {
             var instructions = System.IO.File.ReadAllText("input.txt");
-            Puzzle1(instructions);
-        }
 
-        public static void Puzzle1(string instructions)
-        {
             var floor = 0;
             var steps = 0;
+            bool basementVisisted = false;
             foreach (var ch in instructions)
             {
-                if (ch == '(')
+                switch (ch)
                 {
-                    floor++;
+                    case '(':
+                        floor++;
+                        break;
+                    case ')':
+                        floor--;
+                        break;
                 }
-
-                if (ch == ')')
-                {
-                    floor--;
-                }
-
                 steps++;
-                if (floor == -1)
+
+                if (floor == -1 && !basementVisisted)
                 {
                     Console.WriteLine($"Santa enters basement at step {steps}");
+                    basementVisisted = true;
                 }
             }
             Console.WriteLine($"Santa is at floor #{floor}");
---
+++ CSharp/Day02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2015.Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listPositions = System.IO.File.ReadAllLines("list.txt");
+
+            var totalPaper = 0L;
+            var totalRibbon = 0L;
+
+            foreach (var position in listPositions)
+            {
+                var dimensions = ParseDimensions(position);
+                var paperSize = CalculateSurfaces(dimensions);
+
+                totalPaper += paperSize;
+                var ribbonLength = CalculateRibbonLength(dimensions);
+                totalRibbon += ribbonLength;
+
+            }
+            Console.WriteLine($"The elves must order {totalPaper} square feet of wrapping paper");
+            Console.WriteLine($"They should also order {totalRibbon} feet of ribbon");
+        }
+
+        private static int CalculateRibbonLength(List<int> dimensions)
+        {
+            var max = dimensions.Max();
+            var loop = (dimensions.Sum() - max) * 2;
+            var bow = dimensions.Aggregate(1, (acc, next) => acc * next);
+            return loop + bow;
+        }
+
+        private static int CalculateSurfaces(List<int> dimsensions)
+        {
+            var surfaces =  new List<int>()
+            {
+                dimsensions[0] * dimsensions[1],
+                dimsensions[1] * dimsensions[2],
+                dimsensions[2] * dimsensions[0]
+            };
+            var min = surfaces.Min();
+            var paperSize = surfaces.Sum() * 2 + min;
+            return paperSize;
+        }
+
+        private static List<int> ParseDimensions(string position)
+        {
+            return position.Split("x".ToCharArray()).ToList().ConvertAll(int.Parse);
+        }
+    }
+}
