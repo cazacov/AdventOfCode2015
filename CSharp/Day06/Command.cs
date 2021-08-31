8b4da367c3ec230b64c03e44a1959ef15a9278ae
Victor Cazacov <cazacov.git@gmail.com>
2021-09-01 00:35:23 +0200

    Day 6, puzzles 1 and 2

---
+++ CSharp/Day06/Command.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day06
+{
+    public enum Action
+    {
+        TurnOn,
+        Toggle,
+        TurnOff
+    }
+
+    public class Command
+    {
+        public Action Action { get; private set; }
+        public Point From { get; private set; }
+        public Point To { get; private set; }
+
+        private Command() {}
+
+        public Command(string cmd)
+        {
+            var turnOn = new Regex(@"(turn on|turn off|toggle) (\d*),(\d*) through (\d*),(\d*)");
+            var onMatch = turnOn.Match(cmd);
+            if (onMatch.Success)
+            {
+                switch (onMatch.Groups[1].Value)
+                {
+                    case "turn on":
+                        this.Action = Action.TurnOn;
+                        break;
+                    case "turn off":
+                        this.Action = Action.TurnOff;
+                        break;
+                    case "toggle":
+                        this.Action = Action.Toggle;
+                        break;
+                }
+                
+                this.From = new Point()
+                {
+                    X = Int32.Parse(onMatch.Groups[2].Value),
+                    Y = Int32.Parse(onMatch.Groups[3].Value)
+                };
+                this.To = new Point()
+                {
+                    X = Int32.Parse(onMatch.Groups[4].Value),
+                    Y = Int32.Parse(onMatch.Groups[5].Value)
+                };
+            }
+            else
+            {
+                throw new ApplicationException("Unsupported command: " + cmd);
+            }
+        }
+    }
+
+    public class Point
+    {
+        public int X;
+        public int Y;
+    }
+}
---
+++ CSharp/Day06/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = System.IO.File.ReadAllLines("input.txt");
+
+            Puzzle1(commands);
+            Puzzle2(commands);
+        }
+
+        private static void Puzzle1(string[] commands)
+        {
+            var lights = new bool[1000, 1000];
+            foreach (var str in commands)
+            {
+                var command = new Command(str);
+
+                for (int x = command.From.X; x <= command.To.X; x++)
+                {
+                    for (int y = command.From.Y; y <= command.To.Y; y++)
+                    {
+                        switch (command.Action)
+                        {
+                            case Action.TurnOn:
+                                lights[x, y] = true;
+                                break;
+                            case Action.TurnOff:
+                                lights[x, y] = false;
+                                break;
+                            case Action.Toggle:
+                                lights[x, y] = !lights[x, y];
+                                break;
+                        }
+                    }
+                }
+            }
+
+            var onCount = 0;
+            for (int x = 0; x < 1000; x++)
+            {
+                for (int y = 0; y < 1000; y++)
+                {
+                    if (lights[x, y])
+                    {
+                        onCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{onCount} lights are lit");
+        }
+
+        private static void Puzzle2(string[] commands)
+        {
+            var lights = new int[1000, 1000];
+            foreach (var str in commands)
+            {
+                var command = new Command(str);
+
+                for (int x = command.From.X; x <= command.To.X; x++)
+                {
+                    for (int y = command.From.Y; y <= command.To.Y; y++)
+                    {
+                        switch (command.Action)
+                        {
+                            case Action.TurnOn:
+                                lights[x, y] +=1;
+                                break;
+                            case Action.TurnOff:
+                                if (lights[x, y] > 0)
+                                {
+                                    lights[x, y] -= 1;
+                                }
+                                break;
+                            case Action.Toggle:
+                                lights[x, y] += 2;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            var brightness = 0L;
+            for (int x = 0; x < 1000; x++)
+            {
+                for (int y = 0; y < 1000; y++)
+                {
+                    brightness += lights[x, y];
+                }
+            }
+
+            Console.WriteLine($"Total brightness: {brightness}");
+        }
+    }
+}
