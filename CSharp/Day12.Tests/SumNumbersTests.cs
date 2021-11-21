5266241ae57039325712c9fad3df8b72faea854e
Victor Cazacov <cazacov.git@gmail.com>
2021-11-21 02:29:46 +0100

    Day 12 puzzles

---
+++ CSharp/Day12.Tests/SumNumbersTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Day12.Tests
+{
+    [TestFixture]
+    public class SumNumbersTests
+    {
+        [Test]
+        public void SimpleArray()
+        {
+            var input = "[1,2,3]";
+            var doc = JArray.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void SimpleObject()
+        {
+            var input = "{\"a\":2,\"b\":4}";
+            var doc = JObject.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void NestedArrays()
+        {
+            var input = "[[[3]]]";
+            var doc = JArray.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void NestedObjects()
+        {
+            var input = "{\"a\":{\"b\":4},\"c\":-1}";
+            var doc = JObject.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ObjectWithRedPropertyIsIgnored()
+        {
+            var input = "[1,{\"c\":\"red\",\"b\":2},3]";
+            var doc = JArray.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void ObjectWithRedPropertyIsIgnored2()
+        {
+            var input = "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}";
+            var doc = JObject.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RedElementInArrayHasNoEffect()
+        {
+            var input = "[1,\"red\",5]";
+            var doc = JArray.Parse(input);
+
+            var result = Program.SumNumbers(doc);
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+    }
+}
---
+++ CSharp/Day12/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
+namespace Day12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt");
+
+            Puzzle1(input);
+            Puzzle2(input);
+        }
+
+        private static void Puzzle2(string input)
+        {
+            var doc = JArray.Parse(input);
+            Console.WriteLine(SumNumbers(doc));
+        }
+
+        public static int SumNumbers(JToken doc)
+        {
+            if (doc.Type == JTokenType.Integer)
+            {
+                return doc.Value<int>();
+            }
+
+            if (doc is JProperty pro)
+            {
+                return SumNumbers(pro.Value);
+            }
+
+
+            if (doc is JObject obj)
+            {
+                foreach (var prop in obj.Children<JProperty>())
+                {
+                    if (prop.Value.Type == JTokenType.String)
+                    {
+                        if (prop.Value.Value<string>() == "red")
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            }
+
+            if (doc.Type != JTokenType.Object && doc.Type != JTokenType.Array)
+            {
+                return 0;
+            }
+            
+            var result = 0;
+            foreach (var prop in doc)
+            {
+                result += SumNumbers(prop);
+            }
+            return result;
+        }
+
+        private static void Puzzle1(string input)
+        {
+            var pattern = "-*\\d+";
+            var regex = new Regex(pattern);
+
+            int sum = 0;
+
+            var match = regex.Match(input);
+            while (match.Success)
+            {
+                var val = Int32.Parse(match.Captures[0].Value);
+                sum += val;
+                match = match.NextMatch();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
